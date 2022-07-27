adac71358df5a05af7577c5eb8d62d37ee079f80
Gauthier Bastien <gauthier.bastien@imio.be>
2022-07-27 14:16:32 +0200

    Added field IVideo.cr_preview

--- src/cedes/core/content/video.py
+++ src/cedes/core/content/video.py
@@ -8,12 +8,22 @@
 from AccessControl import ClassSecurityInfo
 from cedes.core.content.lien import ILien
 from cedes.core.content.lien import Lien
+from plone.app.textfield import RichText as RichTextField
+from plone.autoform import directives
 from plone.dexterity.schema import DexteritySchemaPolicy
+from z3c.form.browser.textarea import TextAreaFieldWidget
 from zope.interface import implementer
 
 
 class IVideo(ILien):
     """ """
+
+    directives.widget('cr_preview', TextAreaFieldWidget)
+    directives.order_before(cr_preview='remoteUrl')
+    cr_preview = RichTextField(
+        title='Aperçu',
+        description='Code HTML intégration vidéo',
+        required=False, )
 
 
 @implementer(IVideo)
